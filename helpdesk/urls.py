e5eb9f8b1e12bca53902e72eace5cf9fb12d0e84
Sebastián Aranda Ovallle <sebastian.aranda.o@gmail.com>
2025-10-17 11:55:27 -0300

    commit se agrega carga de imagenes

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from usuarios.views import login, register, profile, logout, MeView, listar_usuarios
 from tickets.views import niveles, areas, cargos, imagenes, mensajes, respuestamensaje
@@ -49,3 +51,6 @@ urlpatterns = [
     path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('', include('usuarios.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
